288bfe20c559177a5ec998cd3bb468e2129e7b0c
qif618 <729785034@qq.com>
2018-03-06 16:42:10 +0800

    提交â

---
+++ src/main/java/com/lyf/common/Const.java
@@ -0,0 +1,8 @@
+package com.lyf.common;
+
+/**
+ * Created by Quincy on 2018/3/6.
+ */
+public class Const {
+    public static final String CURRENT_USER = "currentUser";
+}
---
+++ src/main/java/com/lyf/controller/portal/UserController.java
@@ -0,0 +1,8 @@
+package com.lyf.controller.portal;
+
+/**
+ * Created by Administrator on 2018/3/6.
+ */
+public class UserController {
+
+}
